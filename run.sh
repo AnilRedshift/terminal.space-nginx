6ba8619661185be90c707cfe7eb9cbb2e94b1d98
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-03-10 03:16:25 -0800

    Add better logging

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 set -eo pipefail
 
-aws s3 cp 's3://yatlab-secrets/certs/privkey.pem' '/etc/ssl/private/terminal_space_privkey.pem'
-aws s3 cp 's3://yatlab-secrets/certs/cert.pem' '/etc/ssl/certs/terminal_space_cert.pem'
-aws s3 cp 's3://yatlab-secrets/certs/fullchain.pem' '/etc/ssl/certs/terminal_space_fullchain.pem'
+echo "Downloading certs"
+aws s3 cp 's3://yatlab-secrets/certs/privkey.pem' '/etc/ssl/private/terminal_space_privkey.pem' --sse
+aws s3 cp 's3://yatlab-secrets/certs/cert.pem' '/etc/ssl/certs/terminal_space_cert.pem' --sse
+aws s3 cp 's3://yatlab-secrets/certs/fullchain.pem' '/etc/ssl/certs/terminal_space_fullchain.pem' --sse
+echo "Finished downloading certs, running nginx"
 nginx -g 'daemon off;'
